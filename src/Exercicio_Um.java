c276d93eb7fa8fee651f26fcd138f8664c097075
Igor Oliveira <igoortech@gmail.com>
2019-12-25 23:45:01 -0300

    Exercicio1 concluido

--- src/Exercicio_Um.java
+++ src/Exercicio_Um.java
@@ -5,8 +5,20 @@ public class Exercicio_Um {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner sc = new Scanner(System.in);
+		System.out.println("DIGITE A SENHA DE ACESSO");
+		System.out.println("SENHA CONTENDO 4 DÍGITOS\n");
+		System.out.println("DIGITE A SENHA DE ACESSO");
 		
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+
+		while (x != 2002) {
+			System.out.println("Senha Invalida\n");
+			x = sc.nextInt();
+		}
+
+		System.out.println("\n-----ACESSO PERMITIDO----");
 		sc.close();
 	}
 
